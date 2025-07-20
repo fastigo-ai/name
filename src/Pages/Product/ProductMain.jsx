7fcd9d4e0d76093d2ed786f7b123563732f3174a
Vishal Tiwari <fastigo.in@gmail.com>
2025-07-20 16:13:55 +0530

    first commit3

--- src/Pages/Product/ProductMain.jsx
+++ src/Pages/Product/ProductMain.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import productCategories from "../../Data/productMain";
+import productCategories from "../../Data/ProductMain";
 import { motion } from "framer-motion";
 import Banner from "../../assets/Banner/bannerr14.png";
 import Banner1 from "../../assets/Banner/banner16.png";
